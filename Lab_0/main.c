bb4e19cc2ef0a4d67c5e372e2e5844f6068e4de6
camilleroseee <cvalencia@seattleu.edu>
2022-01-17 13:38:50 -0800

    Updated Lab_0 Output Message
    Updated Lab_0 to output "Now live on GitHub!"

--- Lab_0/main.c
+++ Lab_0/main.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
-    
+        print("Now live on GitHub!\n");
     for(int i = 0;i < argc;i++)
     {
         printf( "%d - %s\n", i, argv[i] );
