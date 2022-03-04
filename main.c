4e05e5334cee33981a367f4cfd7ca9e254fc91f2
camilleroseee <97485410+camilleroseee@users.noreply.github.com>
2022-03-03 20:36:10 -0800

    Lab 4 Program A and B

--- main.c
+++ main.c
@@ -1,174 +1,379 @@
 #include <stdio.h>
-#define INT_SIZE sizeof(int) * 8 // this gives integer size in bits 
+#include <string.h>
+#include <stdlib.h>
 
-// function declarations 
-long Factorial(long num);
-int isPalindrome(long value);
-int HighestBitSet(int value); 
-void ReverseIt(void);
-void ReverseItAgain(void);
-void Pyramid(int lines); 
-int binaryConvert(); 
+// global ptr filePointer will be used to read and write to files 
+FILE *filePointer; 
 
-// Task A - calculate the factorial of the given value and return the value 
-long Factorial(long num){
-    long fact=1;
-    
-    // if/else statement checks whether you can find the factorial or not 
-    // can't find the factorial of anything less than or equal to zero 
-    if(num <=0){
-        return 0; 
-    }
-    // if num > 0, you can find its factorial 
-    else{
-        for(int a = 1; a <=num; a++){
-            fact *= a; 
-        }
-        return fact; 
-    }
-}
-// Task B - determine if the given value’s digits are a palindrome (a palindrome is word, number, phrase, or other sequence of characters which reads the same backward as forward) 
-int isPalindrome(long value){
-    long temp, reverseNum, sumNum = 0;  
-    temp = value;
-    // used a while loop to iterate through each element of the long entry -- will iterate as long as aNumber doesn't equal 0 
-    while(value!=0){
-        // first a modulus 10 operation is performed 
-        sumNum = value % 10;
-        // then reverseNum is multiplied by 10 and added to whatever outputs from previous line 
-        reverseNum = reverseNum * 10 + sumNum; 
-        // then value is divded by 10 to finally reverse the number 
-        value = value/10;
-    }
-    if(reverseNum == temp){
-        printf(" is a PALINDROME!");
-    }
-    else{
-        printf(" is NOT a palindrome!");
-    }
+// Function declarations before main 
+void readStudentRecords(void);
+void printList(void);
+void releaseList(void); 
+void saveList(void);
+
+void updateStud(int id_in);
+void removeStud(int id_in);
+
+void showMenu(void); 
+void showUpdateMenu(char first[30], char last[30]);
+
+// student struct from previous lab -- will be referred to as 'Student' throughout program 
+typedef struct student {
+    int ID;
+    char firstName[30];
+    char lastName[30];
+    float GPA;
+	struct student* next; // next ptr used to implement as linked list 
+} Student;
+
+// makes a student node for each student in the original Student struct 
+Student* makeStudNode(int id_input, char first_input[30], char last_input[30], float gpa_input){ 
+
+	Student* newStud = (Student*) malloc(sizeof(Student)); // allocate memory for each new student 	
+	if(newStud != NULL){
+		newStud->ID = id_input;
+		strcpy(newStud->firstName, first_input);
+		strcpy(newStud->lastName, last_input);
+		newStud->GPA = gpa_input;
+		newStud->next = NULL;
+	}
+	return newStud; // returns the newly created student 
 }
 
-// Task C - determine the position of the highest bit that is set (0 index); Print out the value as a decimal value, in binary and the highest bit set
-int HighestBitSet(int value){
-    int number, order = -1; 
-    for(int i=0; i < INT_SIZE; i++){
-        if((number>>i) & 1){
-            order = i;
-        }
-    }
-    if(order != 1){
-        printf("\n The highest order bit set in %d is %d", number, order); 
-    }
-    else{
-        printf("0 has no bit set"); 
-    }
-    return 0;
+// adds a student to LL 
+void addStudNode(Student* newStud){ 
+	Student* currentStud = initialStud; 
+	if(initialStud == NULL){ 
+		initialStud = newStud; 
+		return; 
+	}
+	while(currentStud->next != NULL){ 
+		currentStud = currentStud->next; 
+	}
+	currentStud->next = newStud; 
+	newStud->next = NULL;  
 }
 
-// made a separate binary converter function that runs before HighestBitSet to provide binary equivalent 
-int binaryConvert(){
-    int a[10], num, y, z;
-    printf("\n Please Enter a Decimal Number You want to Convert  :  ");
-    scanf("%d", &num);
-    
-    for(y = 0; num > 0; y++){
-        a[y] = num % 2;
-        num = num / 2;
-    }
-    
-    printf("\n Binary Equivalent of a Given Number =  ");
-    for(z = y - 1; z >= 0; z--){
-        printf(" %d ", a[z]);
-    }
-    printf("\n");
-    return 0; 
-}
-// Task D - input 5 floating point values. Print them out in reverse order to that in which they were entered; Save the values to a file first in the order they were entered and in reverse order
-void ReverseIt(void){
-    // declare a file pointer called reversefile 
-    FILE *reverseFile;
-    reverseFile = fopen("Lab_2Valencia/ReverseIt.txt","w+");
-    float store[5];
-    // prints the inputted float values 
-    printf("\n Please enter a float value: \n");
-    for(int a = 0; a < 5; a++){
-        float value; 
-        scanf("%f",&value);
-        store[a] = value; 
-        fprintf(reverseFile, "%f\n",value);
-    }
-    // prints the reverse of inputted floats 
-    for(int b = 5; b > 0; b--){
-        printf("%f\n",store[b-1]);
-        fprintf(reverseFile, "%f\n",store[b-1]);
-    }
-}
-// Task E - function should open the file that was saved in (D), read the values and prints them all out.
-void ReverseItAgain(void){
-    FILE *reverseFile; 
-    reverseFile = fopen("Lab_2Valencia/ReverseIt.txt","r"); // the "r" means to read the mentioned file 
-    float n0, n1, n2, n3, n4, n5, n6, n7, n8, n9; 
-    fscanf(reverseFile, "%f %f %f %f %f %f %f %f %f %f", &n0, &n1, &n2, &n3, &n4, &n5, &n6, &n7, &n8,&n9);
-    printf("%f %f %f %f %f %f %f %f %f %f \n", n0, n1, n2, n3, n4, n5, n6, n7, n8,n9); 
+// inserts student input into linked list & sorts student GPAs
+void insertNode(Student* newStud){  
+	Student* node = initialStud;
+    // while loop goes through linked list 
+	while(node->next != NULL){ 
+		Student* nextStud = node->next; 
+        // logic to compare student GPAs
+		if((node->GPA > newStud->GPA) && (newStud->GPA >= nextStud->GPA)){ 
+			newStud->next = node->next; 
+			node->next = newStud; 
+			return; // leaves the logic
+		}		
+		else if(node->GPA <= newStud->GPA){ // Second check for GPA if first one fails 
+			newStud->next = node;
+			initialStud = newStud;
+			return; // leaves the logic 
+		}
+		node = node->next;
+	}
+	node->next = newStud; 
+	newStud->next = NULL;
+	return;	
 }
 
-// Task F - make a pyramid using asterisks depending on number the user inputs + save into a file 
-void Pyramid(int lines){
-    FILE *pyramidFile;
-    // part 1 produces the pyramid itself, size according to user input 
-   int i, space, rows, k = 0;
-   printf("\n Enter the number of rows: ");
-   scanf("%d", &rows);
-   pyramidFile = fopen("Lab_2Valencia/pyramidFile.txt","w+");
-   for (i = 1; i <= rows; ++i, k = 0) {
-      for (space = 1; space <= rows - i; ++space) {
-         printf("  ");
-      }
-      while (k != 2 * i - 1) {
-         printf("* ");
-         ++k;
-      }
-      printf("\n");
-   }
-    for (i = 1; i <= rows; ++i, k = 0) {
-      for (space = 1; space <= rows - i; ++space) {
-         fprintf(pyramidFile,"  ");
-      }
-      while (k != 2 * i - 1) {
-         fprintf(pyramidFile,"* ");
-         ++k;
-      }
-      fprintf(pyramidFile,"\n");
-   }
+// deletes a student based on specific info 
+void deleteStudent(Student* deleteStudent){
+	Student* currentStud = firstStudent; 
+	Student* previousStud = NULL; 
+    // logic to decide if selected student is the first student 
+	if(initialStud == deleteStudent){ 
+		initialStud = initialStud->next; 
+		free(deleteStudent); // freeing up the memory 
+		return; 
+	}
+    // goes through the linked list until selected student is identified 
+	while(currentStud != NULL && currentStud != deleteStudent){ 
+		previousStud = currentStud; 
+		currentStud = currentStud->next; 
+	}
+	if(currentStud != NULL){ 
+		previousStud->next = currentStud->next; 
+	}
+	
+	free(deleteStudent); // Deletes the desired student and frees the its memory.
 }
 
+void readStudentRec(){ 
+	char first_output[30], last_output[30];
+	int num_output, id_output;
+	float gpa_output;
+	
+	filePointer = fopen("StudentRecords.txt", "r"); 
+	fscanf(filePtr, "Number of students: %d", &num_output); 
+	
+	for(int i=0; i<num_output; i++){
+		fscanf(filePtr, "\n\nStudent ID: %d", &id_output);
+		fscanf(filePtr, "\nStudent First Name: %s", first_output);
+		fscanf(filePtr, "\nStudent Last Name: %s", last_output);
+		fscanf(filePtr, "\nStudent GPA: %f", &gpa_output);
+		
+		Student* anotherNode = addStudNode(id_out, first_out, last_out, gpa_out); 
+		addStudNode(anotherNode); 
+	}
+}
+
+// reads the linked list and prints out contents
+void printList(){ 
+	Student* node = initialStud; 
+	printf("\n---STUDENT LINKED LIST---"); 
+	while(node != NULL){ 
+		printf("\n\nStudent ID: %d", node->ID);
+		printf("\nStudent First Name: %s", node->firstName);
+		printf("\nStudent Last Name: %s", node->lastName);
+		printf("\nStudent GPA: %f", node->GPA);
+		node = node->next;
+	}
+	printf("\n\n----"); 
+}
+// goes through the linked list and deletes all the students
+void releaseList(){ 
+	Student* node = initialStud; 
+	Student* nextNode = initialStud->next; 
+	while(node != NULL){ 
+		deleteStudent(node); 
+		node = nextNode; 
+		nextNode = nextNode->next; 
+	}
+}
+
+// writes student details into linked list 
+void saveList(){
+	filePointer = fopen("StudentRecords.txt", "w+"); 
+	Student* node = initialStud; 
+	int studentNum = 0; 
+	while(node != NULL){ 
+		studentNum++; 
+		node = node->next; 
+	}
+	fprintf(filePointer, "Number of students: %d ", studentNum); 
+	node = initialStud; 
+	while(node != NULL){ 
+		fprintf(filePtr, "\n\nStudent ID: %d", node->ID);
+		fprintf(filePtr, "\nStudent First Name: %s", node->firstName);
+		fprintf(filePtr, "\nStudent Last Name: %s", node->lastName);
+		fprintf(filePtr, "\nStudent GPA: %f", node->GPA);
+		node = node->next; // Iterate.
+	}
+	fclose(filePointer); // Close the file.
+}
+
+// gets rid of a student based on their ID 
+void removeStud(int id_input){
+	Student* node = initialStud; 
+	int checker = 0; 
+	while(node != NULL){ 
+		if(id_input == node->ID){
+			char first[30];
+			char last[30];
+			strcpy(first, node->firstName);
+			strcpy(last, node->lastName);
+			deleteStudent(node); 
+			checker = 1; 
+			printf("\n%s %s was successfully deleted.", first, last); 
+			return; 
+		}
+		node = node->next; 
+	}
+	if(checker == 0){
+		printf("\nThere is no student with that specific ID."); 
+		return; 
+	}
+	return; 
+}
+
+// updates student info based on ID input 
+void updateStud(int id_input){ 
+
+	Student* node = initialStud; 
+	Student* previousNode = node; 
+	int check = 0; 
+	int userIn = 0; 
+	while(node != NULL){
+		if(id_in == node->ID){ 
+			do{ 
+				updateStudentMenu(node->firstName, node->lastName); 
+				scanf("%d", &userIn ); 
+				int newID, oldID;
+				char newFirst[30], oldFirst[30];
+				char newLast[30], oldLast[30];
+				float newGPA, oldGPA;
+				Student* idChecker = initialStud;
+				
+				switch(userIn) {
+					case 1 : //1) Updating the ID 
+						printf("\nEnter the new ID: "); 
+						scanf("%d", &newID); 
+						while(idChecker!= NULL){
+							if(newID== idChecker->ID) {
+								printf("\nThis ID already exists");
+								idChecker = initialStud; 
+								printf("\nEnter the ID of the student: "); 
+								scanf("%d", &newID); 
+							}
+							idChecker = idChecker->next; 
+						}
+						oldID = node->ID; 
+						node->ID = newID; 
+						printf("\n%d was sucessfully updated to %d", oldID, newID); 
+						break; 
+					case 2 : // 2) Updating first name 
+						printf("\nEnter the new first name: "); 
+						scanf("%29s", newFirst); 
+						strcpy(oldFirst, node->firstName); 
+						strcpy(node->firstName, newFirst); 
+						printf("\n%s was sucessfully updated to %s", oldFirst, newFirst); 
+						break; 
+					case 3 : // 3) Updating last name 
+						printf("\nEnter the new last name: ");
+						scanf("%29s", newLast); 
+						strcpy(oldLast, node->lastName); 
+						strcpy(node->lastName, newLast);
+						printf("\n%s was sucessfully updated to %s", oldLast, newLast); 
+						break; 
+					case 4 : // 4) Updating GPA
+						printf("\nEnter the new GPA: "); 
+						scanf("%f", &newGPA); 
+						while((newGPA > 4.0) || (newGPA < 1.0)){ 
+							printf("\nPlease enter a GPA between 1.0 and 4.0");
+							printf("\nEnter the GPA of the student: ");
+							scanf("%f", &newGPA); 
+						}
+						oldGPA = node->GPA; 
+						node->GPA = newGPA; 
+						if(previousNode == node){
+							node->next = NULL; 
+							insertNode(node); 
+						}
+						else{
+							previousNode->next = node->next; 
+							insertNode(node); 
+						}
+						printf("\n%f was sucessfully updated to %f", oldGPA, newGPA); 
+						break;
+					
+					case 5 : // 5) Quit
+						break; 
+					
+					default : 
+						printf("\nInvalid input\n");
+				}
+			} while(user_input != 5);
+			return; 
+		}
+		previousNode = node; 
+		node = node->next; 
+	}
+	if(checker == 0) // use for IDs that weren't found 
+	{
+		printf("\nInvalid ID entered."); 
+		return;
+	}
+}
+
+// displays default menu 
+void showMenu(){
+	printf("\n---STUDENT MENU---");
+	
+	printf("\n\n(1) List all entered students");
+	printf("\n(2) Add a Student");
+	printf("\n(3) Remove a Student");
+	printf("\n(4) Update a Student");
+	
+	printf("\n\n(5) Exit the program");
+	printf("\n\n-----");
+	printf("\n\n");
+}
+
+// displays Update menu 
+void showUpdateMenu(char first[30], char last[30]){ 
+	printf("\n---UPDATE MENU---");
+	printf("\nWhat would you like to update about %s %s?", first, last);
+	printf("\n\n(1) Student ID");
+	printf("\n(2)  Student First Name");
+	printf("\n(3) Student Last Name");
+	printf("\n(4) Student GPA");
+	
+	printf("\n\n(5) Exit the program");
+	printf("\n\n-----");
+	printf("\n\n");
+}
 
 int main(){
-    // declares local long variable to test Factorial function 
-    long numTest;
-    printf("Enter a number to find its factorial: ");
-    scanf("%ld",&numTest);
-    printf("The factorial of %ld is: %ld \n",numTest,Factorial(numTest));
-    
-    // declare local long variables to check if they're palindromes 
-    //long value; 
-
-    // ask user for long number to check for palindrome
-   // printf(" \n Please enter a number: ");
-   // scanf("%ld",&value);
-   // isPalindrome(value); 
-    //binaryConvert(); 
-    //int value; 
-    //HighestBitSet(value);
-    
-    //ReverseIt(); 
-    //ReverseItAgain();
-    
-    // calls Pyramid function to produce an asterisk pyramid 
-    //int lines;
-    //Pyramid(lines);
+	int userIn; 
+	
+	readStudentRecords();
+	do{ 
+		int id_input;
+		char first_input[30] = "";
+		char last_input[30] = "";
+		float gpa_input = 0.0;
+		int userID_in; 
+		Student* idCheck = initialStud; 
+		showMenu(); 
+		printf("\nWhat would you like to do? "); 
+		scanf("%d", &userIn); 
+		
+		switch(userIn){ 
+			case 1 : // 1) Reveals list of students
+				readList(); 
+				break; 
+			case 2 : // 2) Adding a student 
+				printf("\nEnter the ID of the student: "); 
+				scanf("%d", &id_input); 
+				while(idCheck != NULL){ 
+					if(id_input == idCheck->ID){ 
+						printf("\nEntered ID is currently used by and existing student."); 
+						idCheck = initialStud; 
+						printf("\nEnter the ID of the student: "); 
+						scanf("%d", &id_input); 
+					}
+					idCheck = idCheck->next; 
+				}
+				printf("\nEnter the first name of the student: "); 
+				scanf("%29s", first_input); 
+				printf("\nEnter the last name of the student: "); 
+				scanf("%29s", last_input); 
+				printf("\nEnter the GPA of the student: "); 
+				scanf("%f", &gpa_input); 
+				while((gpa_input > 4.0) || (gpa_input < 1.0)){ 
+					printf("\nPlease enter a GPA in the range of 4.0 and 1.0."); 
+					printf("\nEnter the GPA of the student: "); 
+					scanf("%f", &gpa_input); 
+				}
+				Student* anotherPtr = addStudNode(id_input, first_input, last_input, gpa_input); 
+				insertNode(anotherPtr); 
+				break; 
+			case 3: // 3) Removing a student 
+				printf("\nEnter the ID of the student you wish to delete: "); 
+				scanf("%d", &userID_in); 
+				removeStud(userID_in);
+				break; 
+			case 4 : // 4) Update Student info 
+				printf("\nEnter the ID of the student you wish to update: "); 
+				scanf("%d", &userID_in); 
+				updateStud(userID_in); 
+				break; 
+			case 5 : // 5) Exiting the program 
+				printf("\nExiting Student Program..."); 
+				break; 
+			default : 
+				printf("\nInvalid input\n"); 
+		}
+	}while(userIn != 5);
+	saveList(); 
+	releaseList(); 
 }
 
- 
+
+
+
+
 
 
